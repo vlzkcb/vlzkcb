31ee66e70abfa2fa2fc8947c4dec6cabbade03cb
vlzkcb <117087367+vlzkcb@users.noreply.github.com>
2022-11-10 01:16:56 +0900

    Time: 12 ms (46.86%), Space: 47.1 MB (21.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,33 +1,42 @@
 class Solution {
     
     static Map<String, Integer> values = new HashMap<>();
-    
+
     static {
-        values.put("M", 1000);
-        values.put("D", 500);
-        values.put("C", 100);
-        values.put("L", 50);
-        values.put("X", 10);
-        values.put("V", 5);
         values.put("I", 1);
+        values.put("V", 5);
+        values.put("X", 10);
+        values.put("L", 50);
+        values.put("C", 100);
+        values.put("D", 500);
+        values.put("M", 1000);
+        values.put("IV", 4);
+        values.put("IX", 9);
+        values.put("XL", 40);
+        values.put("XC", 90);
+        values.put("CD", 400);
+        values.put("CM", 900);
     }
 
     public int romanToInt(String s) {
         
-        String lastSymbol = s.substring(s.length() - 1);
-        int lastValue = values.get(lastSymbol);
-        int total = lastValue;
-
-        for (int i = s.length() - 2; i >= 0; i--) {
-            String currentSymbol = s.substring(i, i + 1);
-            int currentValue = values.get(currentSymbol);
-            if (currentValue < lastValue) {
-                total -= currentValue;
-            } else {
-                total += currentValue;
+        int sum = 0;
+        int i = 0;
+        while (i < s.length()) {
+            if (i < s.length() - 1) {
+                String doubleSymbol = s.substring(i, i + 2);
+                // Check if this is the length-2 symbol case.
+                if (values.containsKey(doubleSymbol)) {
+                    sum += values.get(doubleSymbol);
+                    i += 2;
+                    continue;
+                }
             }
-            lastValue = currentValue;
+            // Otherwise, it must be the length-1 symbol case.
+            String singleSymbol = s.substring(i, i + 1);
+            sum += values.get(singleSymbol);
+            i += 1;
         }
-        return total;
+        return sum;
     }
 }
