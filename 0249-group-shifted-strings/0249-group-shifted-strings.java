cd190dc02028175001606f754146d237bfc2517a
vlzkcb <117087367+vlzkcb@users.noreply.github.com>
2022-12-03 21:09:08 +0900

    Time: 3 ms (84.70%), Space: 44.1 MB (40.93%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -4,7 +4,7 @@ class Solution {
         Map<String, List<String>>map = new HashMap<>();
         for (String str : strings){
             StringBuilder sb = new StringBuilder();
-            char base = str.charAt(0);
+            // char base = str.charAt(0);
             char[] chars = str.toCharArray();
             for (int i = 1, end = str.length(); i < end; i++){
                 // int tp = str.charAt(i) - base >= 0 ? str.charAt(i) - base : 26 + str.charAt(i) - base;
