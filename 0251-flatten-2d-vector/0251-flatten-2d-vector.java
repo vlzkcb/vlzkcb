195a598666aafcf02ceb3d00c49ea84bac790d72
vlzkcb <117087367+vlzkcb@users.noreply.github.com>
2022-12-06 23:18:45 +0900

    Time: 25 ms (33.89%), Space: 51 MB (55.89%) - LeetHub

--- 0251-flatten-2d-vector/0251-flatten-2d-vector.java
+++ 0251-flatten-2d-vector/0251-flatten-2d-vector.java
@@ -1,21 +1,50 @@
 class Vector2D {
-
+    
     int[][] v;
-    int i = 0, j = 0;
+    int row, col;
+    int N;
+    
     public Vector2D(int[][] v) {
         this.v = v;
+        row = 0;
+        col = 0;
+        N = v.length;
     }
     
     public int next() {
-        if (hasNext()) return v[i][j++];
-        else return -1;
+        int ret = 0;
+        if (col < v[row].length) {
+            ret = v[row][col];
+        } else {
+            col = 0;
+            row = nextValidRow(row);
+            ret = v[row][col];
+        }
+        
+        if (col + 1 < v[row].length)
+            col++;
+        else {
+            col = 0;
+            row = nextValidRow(row);
+        }
+        return ret;
     }
     
     public boolean hasNext() {
-        while(i < v.length && j == v[i].length) {  // Move to next available vector
-            i++;
-            j = 0;
+        if (row >= N) return false;
+        if (col < v[row].length) 
+            return true;
+       
+        return (nextValidRow(row) < N);
+    }
+    
+    private int nextValidRow(int r) {
+        r++;
+        while(r < N) {
+            if (v[r].length > 0)
+                return r;
+            r++;
         }
-        return i < v.length;
+        return r;
     }
 }
