af023c63f4de5c20d4a425c8b28e3136b8831769
vlzkcb <117087367+vlzkcb@users.noreply.github.com>
2022-12-16 21:31:38 +0900

    Time: 1118 ms (5.05%), Space: 42.3 MB (95.19%) - LeetHub

---
+++ 0277-find-the-celebrity/0277-find-the-celebrity.java
@@ -0,0 +1,34 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        boolean[] isCele = new boolean[n];
+        Arrays.fill(isCele, true);
+        
+        for(int i = 0; i < n; i++){
+            // if (!isCele[i]) continue;
+            for(int j = 0; j < n; j++){
+                if (i != j && knows(i,j)) isCele[i] = false;
+                if (i != j && knows(j,i)) isCele[j] = false;
+            }
+            for(int j = 0; j < n; j++){
+                if(isCele[j]){
+                    if(!knows(i, j)) isCele[j] = false;
+                }
+            }
+            System.out.println(Arrays.toString(isCele));
+        }
+        int idx = -1;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (isCele[i]) {
+                cnt++;
+                idx = i;
+            }
+        }
+        if (cnt != 1) return -1;
+        return idx;
+        // return -1;
+    }
+}
